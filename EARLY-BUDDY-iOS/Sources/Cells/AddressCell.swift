ff59742630d20ac2191780b9b066dfd488ae7da1
khyunjiee <khyunjiee@gmail.com>
2020-01-03 01:11:09 +0900

    [Schedule] 장소 검색 진행중

---
+++ EARLY-BUDDY-iOS/Sources/Cells/AddressCell.swift
@@ -0,0 +1,9 @@
+//
+//  AddressCell.swift
+//  EARLY-BUDDY-iOS
+//
+//  Created by 김현지 on 2020/01/02.
+//  Copyright © 2020 김현지. All rights reserved.
+//
+
+import Foundation
---
+++ EARLY-BUDDY-iOS/Sources/ViewControllers/Schedule/SearchAddressViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchAddressViewController.swift
+//  EARLY-BUDDY-iOS
+//
+//  Created by 김현지 on 2020/01/02.
+//  Copyright © 2020 김현지. All rights reserved.
+//
+
+import UIKit
+
+class SearchAddressViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
